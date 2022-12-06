3d1ca96b7e7327a0ea70d4d464b7f9a23e8cc803
Krys Frost <librarykrystal@gmail.com>
2022-12-05 19:15:37 -0600

    Inputs and buttons created. POST and GET functions written in client.js.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,50 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit').on('click', submitIt);
+$('#restart').on('click', restartIt);
+}
+
+let guesses;
+
+function submitIt(){
+  guesses = {
+    Allie: $('#guessAllie').val(),
+    David: $('#guessDavid').val(),
+    Krystal: $('#guessKrystal').val(),
+  }
+  postGuesses();
+}
+
+function postGuesses(){
+  console.log('f postGuesses TEST');
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: guesses,
+  }).then(function(response){
+    console.log('response from server', response)
+    getResult();
+  }).catch(function(error){
+    alert(error.responseText);
+  })
+}
+
+function getResult(){
+  console.log('f getResults TEST');
+  $.ajax({
+    method: 'GET',
+    url: '/guesses',
+  }).then(function(response){
+    console.log('Response from server', response);
+    appendToDom(response);
+  })
+}
+
+appendToDom(){
+  
+}
+
+function restartIt(){
+    
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
+
+const guessHistory = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -11,7 +13,14 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+app.get("/guesses", function (req,res){
+  console.log ("request for /guesses made", req.body);
+  res.send(guessHistory);
+})
 
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
